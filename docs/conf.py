f8542f088d27ce50ef3c43e20473568e9af301fc
speleophysics <speleophysics@gmail.com>
2014-12-28 22:49:19 -0600

    forgot to save conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,15 +13,15 @@
 
 import sys, os
 
-#snippet to mock libraries so that docs will compile
-from mock import Mock as MagicMock
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return Mock()
+##snippet to mock libraries so that docs will compile
+#from mock import Mock as MagicMock
+#class Mock(MagicMock):
+#    @classmethod
+#    def __getattr__(cls, name):
+#            return Mock()
 
-MOCK_MODULES = ['numpy', 'pandas']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#MOCK_MODULES = ['numpy', 'pandas']
+#sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 #end mock snippet
 
 # If extensions (or modules to document with autodoc) are in another directory,
