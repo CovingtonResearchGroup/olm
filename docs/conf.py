a224fca487b3771893d6402d714f9209cf116973
speleophysics <speleophysics@gmail.com>
2014-12-28 22:47:24 -0600

    removed mock packages
    try using install site packages option on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['numpy', 'pandas','scipy.optimize']
+MOCK_MODULES = ['numpy', 'pandas']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 #end mock snippet
 
