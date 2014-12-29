1b3dd1db2680cc3ee56a024d94a3db772eeb14a7
speleophysics <speleophysics@gmail.com>
2014-12-28 22:02:15 -0600

    added setup.py
    Added the setup script and also made a few edits to try to get
    readthedocs compile to work.

---
+++ waterchem/setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from disutils.core import setup
+setup(name='waterchem',
+    version='0.33',
+    author='Matt Covington',
+    url='https://github.com/speleophysics/waterchem/',
+    packages=['waterchem', 'waterchem.USGS'],
+    )
