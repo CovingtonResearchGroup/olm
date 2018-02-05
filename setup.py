313a08527e47e83fe8a5df10d6ea4906d14988c3
Matt Covington <speleophysics@gmail.com>
2018-02-05 15:11:38 -0600

    updated version number
    Addition of dissRateFromCaPCO2 to ease calculation of dissolution rates and enable Monte Carlo error propagation.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 
 setup(name='olm',
-    version='0.33',
+    version='0.34',
     author='Matt Covington',
     url='https://github.com/speleophysics/olm/',
     packages=['olm', 'olm.USGS', 'olm.loggers'],
