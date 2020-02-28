7a4634a7fe78874e5163d96c551dada8aefb1ded
Matt Covington <speleophysics@gmail.com>
2020-02-28 16:53:04 +0100

    first attempt at cythonizing calcite

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 from distutils.core import setup
-
+from Cython.Build import cythonize
 
 setup(name='olm',
     version='0.35',
     author='Matt Covington',
     url='https://github.com/speleophysics/olm/',
     packages=['olm', 'olm.USGS', 'olm.loggers'],
+    ext_modules = cythonize('olm/calcite.pyx')
     )
     
