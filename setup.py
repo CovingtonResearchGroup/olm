02a29a3f3766567b79d282e622b8e834192f957b
Matt Covington <speleophysics@gmail.com>
2020-09-15 13:56:41 +0200

    updated docs and setup to version 0.36

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 
 setup(name='olm',
-    version='0.35',
+    version='0.36',
     author='Matt Covington',
     url='https://github.com/speleophysics/olm/',
     packages=['olm', 'olm.USGS', 'olm.loggers'],
