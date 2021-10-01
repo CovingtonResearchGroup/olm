1a6f1fca9530636d42fbc6d52b949fd60ae8cdb2
speleophysics <speleophysics@gmail.com>
2021-10-01 09:55:57 -0500

    updated setup files for pip

--- setup.py
+++ setup.py
@@ -3,9 +3,21 @@ from distutils.core import setup
 
 
 setup(name='olm',
-    version='0.36',
-    author='Matt Covington',
+    version='0.37',
+    author='Matthew D. Covington',
     url='https://github.com/speleophysics/olm/',
+      download_url = 'https://github.com/CovingtonResearchGroup/olm/archive/refs/tags/0.37.tar.gz',
     packages=['olm', 'olm.USGS', 'olm.loggers'],
+      install_requires = [numpy,
+                          scipy,
+                          matplotlib,
+                          pandas,
+                          lxml,
+                          xlrd,
+                          xlwt,
+                          openpyxl,
+                          requests,
+      ],
+      
     )
     
